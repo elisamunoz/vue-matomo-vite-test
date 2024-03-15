4f91d4905039c17cf13cfe90d5039adef43467bd
ElisaMunoz <elisa@bable-smartcities.eu>
2024-03-15 16:24:23 +0100

    added matomo config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import VueMatomo from "vue-matomo";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(VueMatomo, {
+  // Configure your Matomo server and site by providing:
+  host: "https://elisa-github.matomo.cloud/",
+  siteId: 1,
+});
+app.mount("#app");
+window._paq.push(["trackPageView"]); // To track a page view
